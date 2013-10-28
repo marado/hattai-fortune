ea0f5ad76b59997c3f91277ea5ec95d0fc0deca0
Nuno Nunes <nuno@nunonunes.org>
2013-10-28 22:33:06 +0000

    Primeira versão a funcionar da mesma forma que a versão anterior em shell.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ hattai-fortune
 
 Hattai's Fortune: a script to be used by a bot, that gives news (from Público)
 
-## Scripts behaviour
+## Script's behaviour
 
 This is the behaviour of the script, as extracted from the previous implementation.
 
@@ -12,21 +12,8 @@ When run the script will:
 1. Read the RSS feed of the Público newspaper
    (http://http://feeds.feedburner.com/publicoRSS)
 2. Exclude the article with the same title as the one from the previous run
-3. Exclude the articles which contain any of the following words in the title
-   (the match is case **insensitive**):
-    - '*blico*'
-    - '*psilon*'
-    - '*benfic*'
-    - '*assinant*'
-    - '*sporting*'
-    - '*chelsea*'
-    - '*arsenal*'
-    - '*derby*'
-    - '*golo*'
-    - 'djokovic*'
-    - '*jogo*'
-    - '*ronaldo*'
-    - '*lt;*'
+3. Exclude the articles which contain any word from a given list of "bad words"
+   (the match is case **insensitive**)
 4. Exclude articles which have an empty title
 5. Exclude articles which have the character '"' in the title
 6. Pick off the first article from the remaining articles list and:
--- hattai-fortune.py
+++ hattai-fortune.py
@@ -1,15 +1,73 @@
+#!/usr/bin/env python
+
+# hattai-fortune
+#
+# Original author: Mindbooster Noori
+# Author of python version: Nuno Nunes <nuno@nunonunes.org>
+
+
 import sys
-
-sys.path.append('./feedparser')
-
+sys.path.append("./feedparser")
 import feedparser
 
+# Configuration
+#
+feed_url = "http://feeds.feedburner.com/publicoRSS"
+title_file_name = "title"
+link_file_name = "link"
+#
+################
 
-feed = feedparser.parse('http://feeds.feedburner.com/publicoRSS')
-print "Got " + str(len(feed.entries)) + " entries in the feed"
+bad_words = [
+    "olhanense",
+    "psilon",
+    "benfic",
+    "assinant",
+    "sporting",
+    "chelsea",
+    "arsenal",
+    "derby",
+    "golo",
+    "djokovic",
+    "jogo",
+    "ronaldo",
+    "lt;",]
 
+try:
+    title_file = open(title_file_name, "r")
+    previous_title = title_file.readline()
+    title_file.close()
+except:
+    previous_title = ""
+
+
+feed = feedparser.parse(feed_url)
+articles = []
 for post in feed.entries:
-    print post.title
-    print post.link
-    print
+    post.title = post.title.encode("utf-8")
+    post.link  = post.link.encode("utf-8")
+    if post.title == previous_title:
+        continue
+    if post.title == "":
+        continue
+    if '"' in post.title:
+        continue
+    has_bad_words = False
+    for bad_word in bad_words:
+        if bad_word in post.title.lower():
+            has_bad_words = True
+            continue
+    if has_bad_words:
+        continue
+    articles.append( [post.title,post.link] )
 
+try:
+    title_file = open( title_file_name, "w" )
+    link_file  = open( link_file_name, "w" )
+    title_file.write( articles[0][0] )
+    link_file.write( articles[0][1] )
+    title_file.close()
+    link_file.close()
+    print articles[0][0]
+except:
+    print "BORK! : " + sys.exc_info()[0]
